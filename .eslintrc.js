0ddd25e7e81484beb0803727144c0e586b087ba9
Ben <3126319718@qq.com>
2021-10-16 18:57:54 +0800

    init commit.

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+  },
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+# Extension Template
+
+This is a template for extension building by Vue3 + Vite + Tailwindcss.
+
+## Recommended IDE Setup
+
+- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)the
